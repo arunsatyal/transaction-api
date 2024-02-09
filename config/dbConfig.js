de38b0c10d38aa8bff80fd817e8185374cd79186
Arun <arunsatyal@hotmail.com>
2024-02-10 10:10:08 +1100

    db config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
-const mongo_db_url = "mongodb://localhost:27017/transaction-db"
+const mongo_db_url =
+  process.env.mongo_db_url || "mongodb://localhost:27017/transaction-db";
 
 export const connectMongo = () => {
   try {
-    const connect = mongoose.connect(mongo_db_url)
-    if(connect) {
+    const connect = mongoose.connect(mongo_db_url);
+    if (connect) {
       console.log("Database conected");
     }
   } catch (error) {
     console.log("Error:", error);
   }
-}
+};
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { connectMongo } from "./config/dbConfig.js";
